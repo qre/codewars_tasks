733ec60c486198e6690a62c22bcb56690c3b627f
qre <zaikingleb@gmail.com>
2022-03-31 10:49:46 +0200

    Update friend.py

--- friend.py
+++ friend.py
@@ -1,11 +1,12 @@
+#my solution:
 def friend(x):
-    #Code
     names = []
     for name in x:
         if len(name) == 4:
             names.append(name)
     return names
 
+#best voted solutions:
 def friend(x):
     return [f for f in x if len(f) == 4]
 
