538a229582ab3e195a7d73b8a49c9fd4915e0165
qre <zaikingleb@gmail.com>
2022-03-31 10:52:03 +0200

    Update sum.py

--- sum.py
+++ sum.py
@@ -7,13 +7,3 @@ def summation(num):
 def summation(num):
     return sum(range(num + 1))
 
-# In Python 2.x:
-
-# range creates a list, so if you do range(1, 10000000) it creates a list in memory with 9999999 elements.
-
-# xrange is a sequence object that evaluates lazily.
-
-# In Python 3:
-
-# range does the equivalent of Python 2's xrange. To get the list, you have to explicitly use list(range(...)).
-# xrange no longer exists.
