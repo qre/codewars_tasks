e3f07424439d3bbd4f75295d0a85fb37a6fbd5c6
qre <zaikingleb@gmail.com>
2022-03-31 10:46:33 +0200

    Update division_test.py

--- division_test.py
+++ division_test.py
@@ -1,8 +1,10 @@
+#my solution:
 def is_divide_by(number, a, b):
     if number % a == 0 and number % b == 0:
         return True
     else:
         return False
 
+    #best voted solution:
 def is_divide_by(number, a, b):
     return number % a == 0 and number % b == 0
